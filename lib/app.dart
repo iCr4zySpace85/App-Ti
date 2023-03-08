333f1e4dce62847be21bf0e735dc5b1f90abda0d
iCr4zySpace85 <iCr4zySpace85@gmail.com>
2023-03-07 18:35:08 -0600

    pus lo ultimo en apksxd

--- lib/app.dart
+++ lib/app.dart
@@ -4,10 +4,10 @@ class LoginPage extends StatefulWidget {
   static String id = 'login_page';
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<LoginPage> createState() => Inicio();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -34,6 +34,10 @@ class _LoginPageState extends State<LoginPage> {
             height: 20,
           ),
           _bottonLogin(),
+          SizedBox(
+            height: 20,
+          ),
+          _recuperar(),
           SizedBox(
             height: 20,
           ),
@@ -92,7 +96,8 @@ class _LoginPageState extends State<LoginPage> {
             Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => SecondRoute()),
-        );},
+        );
+        },
       );
     });
   }
@@ -104,17 +109,40 @@ Widget _recuperar() {
       return ElevatedButton(
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-          child: Text('Iniciar Sesión',
+          child: Text('Recuperar contraseña',
               style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold)),
         ),
         onPressed: () {
             Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => SecondRoute()),
+            MaterialPageRoute(builder: (context) => Route3()),
         );},
       );
     });
   }
+
+
+class Inicio extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Inicio"),
+      ),
+      body: Center(              
+        child: ElevatedButton(
+          onPressed: () {
+             Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => _LoginPageState()),
+        );
+          },
+          child: Text('Regresar'),
+        ),
+      ),
+    );
+  }
+}
 
 
 
@@ -123,17 +151,73 @@ class SecondRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Inicio"),
+        title: Text("Registros"),
       ),
-      body: Center(
+      body: Center(              
         child: ElevatedButton(
           onPressed: () {
             Navigator.pop(context);
           },
-          child: Text('Go back!'),
+          child: Text('Regresar'),
         ),
       ),
     );
+  }
+}
+
+class Route3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Recuperar contraseña"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _userTextField(),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+             mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+              onPressed: () {},
+              child: Text('Enviar'),
+              ),
+              SizedBox(width: 40),
+              ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Cancelar'),
+        ),
+            ],
+          ),
+          
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _userTextField() {
+    return StreamBuilder(builder: (BuildContext, AsyncSnapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 35.0),
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            icon: Icon(Icons.email),
+            hintText: 'ejemplo@correo.com',
+            labelText: 'Correo electronico',
+          ),
+          onChanged: (value) {},
+        ),
+      );
+    });
   }
 }
 
